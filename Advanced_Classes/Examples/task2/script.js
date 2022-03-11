38b694781c9e7d03a933d3fa78ec85ffcad5e62a
Shelby Rothman <srothman@Sroth-7PZB163.corelation.local>
2022-03-11 13:04:23 -0800

    solved script to remove files older than 1 week

--- Advanced_Classes/Examples/task2/script.js
+++ Advanced_Classes/Examples/task2/script.js
@@ -1,2 +1,27 @@
-// script to clean old files in a directory
+// script to clean old files in a directory called files
 // anything older than 7 days should be deleted
+
+const fs = require('fs');
+const path = require('path');
+const dirname = path.join(__dirname, 'files');
+
+const files = fs.readdirSync(dirname);
+
+files.forEach(file => {
+
+    // check the timestamp for each file and if it is older than 7 days, delete it
+    const filePath = path.join(dirname, file);
+    //! always important to use filePath and not a string so it can be cross platform
+    fs.stat(filePath, (err, stats) => {
+        if (err) throw err;
+
+        const timeDiff = Date.now() - stats.mtimeMs;
+        if (timeDiff > 7 * 24 * 60 * 60 * 1000) {
+            fs.unlink(filePath, (err) => {
+                if (err) throw err;
+                console.log(`The ${filePath} has been deleted!`);
+            });
+        }
+    });
+
+})
--- Advanced_Classes/Examples/task2/seed.js
+++ Advanced_Classes/Examples/task2/seed.js
@@ -13,16 +13,20 @@ const ms1Day = 24 * 60 * 60 * 1000;
 for (let i = 0; i < 10; i++) {
     // we want every file to have a diffferent timestamp
     // starting with the current and going back one at a time
-    const filePath = path.join(dirname, `file${i}.txt`);
+    const filePath = path.join(dirname, `file${i}`);
 
-    fs.writeFile(filePath, i, (err) => {
+    // convert the i into a string from a number (so can ue it inside the writeFIle())
+    const iString = i.toString();
+
+
+
+    fs.writeFile(filePath, iString, (err) => {
         if (err) throw err;
         const time = (Date.now() - i * ms1Day) / 1000;
 
         // utimes changes the time stamp of the file
         fs.utimes(filePath, time, time, (err) => {
             if (err) throw err;
-            console.log('The file has been created!');
         });
     });
 
