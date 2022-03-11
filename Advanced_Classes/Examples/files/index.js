0ca564cca0b1ea7c1432e21bce568dd66e7293e8
Shelby Rothman <srothman@Sroth-7PZB163.corelation.local>
2022-03-11 12:40:49 -0800

    much more efficient solution added

---
+++ Advanced_Classes/Examples/files/index.js
@@ -0,0 +1,8 @@
+const dns = require('dns');
+
+// put argument into a variable
+const domain = process.argv[2];
+
+dns.lookup(domain || 'localhost', (err, address, family) => {
+    console.log('address: %j family: IPv%s', address, family);
+});
--- Advanced_Classes/Examples/files/net.js
+++ Advanced_Classes/Examples/files/net.js
@@ -9,14 +9,3 @@ net.connect(8000, () => {
 }
 
 net.end();
-const net = require('net');
-
-net.createServer((socket) => {
-    socket.end('goodbye\n');
-}
-
-net.connect(8000, () => {
-    console.log('connected to server');
-}
-
-net.end();
